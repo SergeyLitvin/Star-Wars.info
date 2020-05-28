726e7e3aa2b6994d4b265e54d21f3f4bb0394216
Sergey Litvin <sergeylitvinc4w@gmail.com>
2020-05-29 00:09:32 +0300

    Create async function getResource and get test data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello Djeday');
+console.log('Hello JS Djeday');
+
+const getResource = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+}
+
+getResource('https://swapi.dev/api/people/1')
+    .then((data) => {
+        console.log('Luke Skywalker', data);
+    })
