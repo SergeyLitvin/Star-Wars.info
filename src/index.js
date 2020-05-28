b5a855ec20e1f7c5f602967d686e789f7ad8bf7e
Sergey Litvin <sergeylitvinc4w@gmail.com>
2020-05-29 00:25:44 +0300

    Improvement function getResourced. Adding request error handling

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@ console.log('Hello JS Djeday');
 
 const getResource = async (url) => {
     const res = await fetch(url);
+    if(!res.ok) {
+        throw new Error(` ${url}, received ${res.status}`);
+    }
+
+
     const data = await res.json();
     return data;
 }
 
-getResource('https://swapi.dev/api/people/1')
+getResource('https://swapi.dev/api/people/11')
     .then((data) => {
-        console.log('Luke Skywalker', data);
+        console.log('Response data', data);
+    })
+    .catch((err) => {
+        console.error('Coud not fetch', err);
     })
