c6e8c05a1706e868e37150ac22e46655800d3998
Sergey Litvin <sergeylitvinc4w@gmail.com>
2020-05-30 01:42:30 +0300

    Create SwapiService for getting data about people, planets and starships.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,74 @@
 console.log('Hello JS Djeday');
 
-const getResource = async (url) => {
-    const res = await fetch(url);
-    if(!res.ok) {
-        throw new Error(` ${url}, received ${res.status}`);
+class SwapiService {
+    _baseUrl = `https://swapi.dev/api/`;
+
+    async getResource(url) {
+        const res = await fetch(this._baseUrl + url);
+        if(!res.ok) {
+            throw new Error(` ${url}, received ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
     }
 
+    async getAllPeople(url) {
+        const res = await this.getResource('people/');
+        return res.results;
+    }
 
-    const data = await res.json();
-    return data;
+    async getPerson(id) {
+        const peopleId = parseInt(id, 10);
+        if(typeof peopleId !== 'number' || isNaN(peopleId)) {
+            console.warn('People id must be a number!');
+        } else {
+            const res = await this.getResource(`people/${peopleId}`);
+            return res;
+        }
+
+    }
+
+    async getAllPlanets(url) {
+        const res = await this.getResource('planets/');
+        return res.results;
+    }
+
+    async getPlanet(id) {
+        const planetId = parseInt(id, 10);
+        if(typeof planetId !== 'number' || isNaN(planetId)) {
+            console.warn('Planet id must be a number!');
+        } else {
+            const res = await this.getResource(`planets/${planetId}`);
+            return res;
+        }
+    }
+
+    async getAllStarships(url) {
+        const res = await this.getResource('starships/');
+        return res.results;
+    }
+
+    async getStarship(id) {
+        const starshipId = parseInt(id, 10);
+        if(typeof starshipId !== 'number' || isNaN(starshipId)) {
+            console.warn('Starship id must be a number!');
+        } else {
+            const res = await this.getResource(`starships/${starshipId}`);
+            return res;
+        }
+    }
 }
 
-getResource('https://swapi.dev/api/people/11')
-    .then((data) => {
-        console.log('Response data', data);
-    })
-    .catch((err) => {
-        console.error('Coud not fetch', err);
+const getResService = new SwapiService();
+
+getResService.getAllPeople()
+    .then((people) => {
+        people.forEach((item) => {
+            console.log(item.name);
+        })
+    });
+
+getResService.getPlanet('12')
+    .then((res) => {
+        console.log(res);
     })
