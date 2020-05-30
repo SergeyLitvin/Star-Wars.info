3f10e2f61dbdeda4c0bf2b870314db49e950f56f
Sergey Litvin <sergeylitvinc4w@gmail.com>
2020-05-30 14:16:09 +0300

    Create empty files for all components.

---
+++ src/services/swapi-service.js
@@ -0,0 +1,58 @@
+export default class SwapiService {
+    _baseUrl = `https://swapi.dev/api/`;
+
+    async getResource(url) {
+        const res = await fetch(this._baseUrl + url);
+        if(!res.ok) {
+            throw new Error(`${url}, received ${res.status}`);
+        }
+        const data = await res.json();
+        return data;
+    }
+
+    async getAllPeople(url) {
+        const res = await this.getResource('people/');
+        return res.results;
+    }
+
+    async getPerson(id) {
+        const peopleId = parseInt(id, 10);
+        if(typeof peopleId !== 'number' || isNaN(peopleId)) {
+            console.warn('People id must be a number!');
+        } else {
+            const res = await this.getResource(`people/${peopleId}`);
+            return res;
+        }
+
+    }
+
+    async getAllPlanets(url) {
+        const res = await this.getResource('planets/');
+        return res.results;
+    }
+
+    async getPlanet(id) {
+        const planetId = parseInt(id, 10);
+        if(typeof planetId !== 'number' || isNaN(planetId)) {
+            console.warn('Planet id must be a number!');
+        } else {
+            const res = await this.getResource(`planets/${planetId}`);
+            return res;
+        }
+    }
+
+    async getAllStarships(url) {
+        const res = await this.getResource('starships/');
+        return res.results;
+    }
+
+    async getStarship(id) {
+        const starshipId = parseInt(id, 10);
+        if(typeof starshipId !== 'number' || isNaN(starshipId)) {
+            console.warn('Starship id must be a number!');
+        } else {
+            const res = await this.getResource(`starships/${starshipId}`);
+            return res;
+        }
+    }
+}
